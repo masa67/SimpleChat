1bc5fa43b086cd4816acd41f66ea37cae2854986
Matti Sangi <matti.sangi@pp.inet.fi>
2015-09-27 12:26:45 +0300

    Original

---
+++ server/server.js
@@ -0,0 +1,12 @@
+
+'use strict';
+
+var http = require('http');
+
+http.createServer(function (req, resp) {
+    resp.writeHead(200, {'Content-Type': 'text-plain'});
+
+    resp.end('Hello World\n');
+}).listen(8081);
+
+console.log('Server running at http://127.0.0.1:8081/');
